d7e7ef790f0293b3bbec905c33b877e80f9ee947
Marouen <9755388+Marujah@users.noreply.github.com>
2023-11-15 15:13:26 +0100

    Update mmv-stage-helper.js

--- assets/mmv-stage-helper.js
+++ assets/mmv-stage-helper.js
@@ -1,15 +1,12 @@
-if ('serviceWorker' in navigator) {
-    // Register a service worker hosted at the root of the
-    // site using the default scope.
-    navigator.serviceWorker.register('sw.js').then(
-      (registration) => {
-        console.log('Service worker registration succeeded:', registration);
-      },
-      (error) => {
-        console.error(`Service worker registration failed: ${error}`);
-      },
-    );
-  } else {
-    console.error('Service workers are not supported.');
+const _fetch = window.fetch;
+window.fetch = (url, options) => {
+  if (
+    url ===
+    'https://int.api.oneweb.mercedes-benz.com/me/vsc/v1/user/vehicles?&locale=de-DE'
+  ) {
+    return _fetch('http://localhost:4000', options);
   }
+
+  return _fetch(url, options);
+};
   
